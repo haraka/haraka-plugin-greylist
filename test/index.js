26dd05aea8080ba284a294be37b8f389e3e9ce11
Matt Simerson <matt@tnpi.net>
2020-07-30 16:15:53 -0700

    repackaged as NPM module

--- test/index.js
+++ test/index.js
@@ -1,36 +1,53 @@
+'use strict'
+const assert = require('node:assert')
+const { beforeEach, describe, it } = require('node:test')
 
-// node.js built-in modules
-const assert   = require('assert')
-
-// npm modules
+const path = require('path')
 const fixtures = require('haraka-test-fixtures')
+const ipaddr = require('ipaddr.js')
 
-// start of tests
-//    assert: https://nodejs.org/api/assert.html
-//    mocha: http://mochajs.org
+const _set_up = (t, done) => {
+  this.plugin = new fixtures.plugin('greylist')
+  this.plugin.config.root_path = path.resolve(__dirname, '../../config')
 
-beforeEach(function (done) {
-  this.plugin = new fixtures.plugin('template')
-  done()  // if a test hangs, assure you called done()
-})
+  this.plugin.register()
+  this.plugin.whitelist = {
+    mail: { 'josef@example.com': true },
+    rcpt: { 'josef@example.net': true },
+    ip: [
+      ipaddr.parseCIDR('123.123.123.234/32'),
+      ipaddr.parseCIDR('2a02:8204:d600:8060:7920:4040:20ee:9680/128'),
+      ipaddr.parseCIDR('123.210.123.234/27'),
+      ipaddr.parseCIDR('2a02:8204:d600:8060:7920:eeee::/96'),
+    ],
+  }
+  this.plugin.list = { dyndom: ['sgvps.net'] }
 
-describe('template', function () {
-  it('loads', function (done) {
-    assert.ok(this.plugin)
-    done()
+  this.connection = fixtures.connection.createConnection()
+  this.connection.init_transaction()
+
+  done()
+}
+
+describe('greylist', () => {
+  beforeEach(_set_up)
+
+  it('inlist: mail(1)', () => {
+    assert.ok(this.plugin.addr_in_list('mail', 'josef@example.com'))
   })
-})
 
-describe('load_template_ini', function () {
-  it('loads template.ini from config/template.ini', function (done) {
-    this.plugin.load_template_ini()
-    assert.ok(this.plugin.cfg)
-    done()
+  it('inlist: rcpt(1)', () => {
+    assert.ok(this.plugin.addr_in_list('rcpt', 'josef@example.net'))
   })
 
-  it('initializes enabled boolean', function (done) {
-    this.plugin.load_template_ini()
-    assert.equal(this.plugin.cfg.main.enabled, true, this.plugin.cfg)
-    done()
+  it('inlist: dyndom(1)', () => {
+    assert.ok(this.plugin.domain_in_list('dyndom', 'sgvps.net'))
+  })
+
+  it('inlist: ip(4)', () => {
+    assert.ok(this.plugin.ip_in_list('123.123.123.234'))
+    assert.ok(this.plugin.ip_in_list('123.210.123.234'))
+    assert.ok(this.plugin.ip_in_list('2a02:8204:d600:8060:7920:4040:20ee:9680'))
+    assert.ok(this.plugin.ip_in_list('2a02:8204:d600:8060:7920:eeee::ff00'))
   })
 })
